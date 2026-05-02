150184931bc014c1564635306d18abaf6bed8dda
Aleksandr <alexandr.makarov.2000@gmail.com>
2026-05-02 16:48:56 +0300

    feat: add get items endpoint

---
+++ Cloudy.API/Endpoints/Items/Get/GetItemsEndpoint.cs
@@ -0,0 +1,33 @@
+﻿using Carter;
+using Cloudy.API.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cloudy.API.Endpoints.Items.Get;
+
+public sealed class GetItemsEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("api/items", async (
+                [FromServices] ApplicationDbContext dbContext,
+                CancellationToken cancellationToken
+            ) =>
+            {
+                var items = await dbContext.Items
+                    .AsNoTracking()
+                    .OrderByDescending(x => x.CreatedAt)
+                    .Select(x => new ItemResponse(
+                        x.Id,
+                        $"{x.Name}.{x.Extension}",
+                        x.ContentType,
+                        x.Size,
+                        x.CreatedAt
+                    ))
+                    .ToListAsync(cancellationToken);
+
+                return Results.Ok(items);
+            })
+            .WithTags(Tags.Items);
+    }
+}
---
+++ Cloudy.API/Endpoints/Items/Get/ItemResponse.cs
@@ -0,0 +1,3 @@
+﻿namespace Cloudy.API.Endpoints.Items.Get;
+
+public record ItemResponse(Guid Id, string DisplayName, string ContentType, long Size, DateTime CreatedAt);
